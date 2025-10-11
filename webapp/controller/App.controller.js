e9f5d2c9396563e5e8aaa3808746ccf1773fbf90
CodeWidGuru <180097088+Code-wid-Guru@users.noreply.github.com>
2025-10-11 18:28:08 +0530

    added theme toggle

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,5 +2,18 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller"
 ], function (Controller) {
   "use strict";
-  return Controller.extend("SapDevPortfolio.controller.App", {});
+  return Controller.extend("SapDevPortfolio.controller.App", {
+    onInit: function() {
+      var oModel = new sap.ui.model.json.JSONModel({
+        themeImage: "images/light_mode.png" // default image path
+      });
+      this.getView().setModel(oModel);
+    },
+    onToggleTheme: function(oEvent) {
+      console.log("Toggle button pressed");
+      var bPressed = oEvent.getParameter("state");
+      var sTheme = bPressed ? "sap_horizon" : "sap_horizon_dark";
+      sap.ui.getCore().applyTheme(sTheme);
+    }
+  });
 });
