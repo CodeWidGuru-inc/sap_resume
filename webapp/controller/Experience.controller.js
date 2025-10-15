ddd791d1e5e07f64851d8dc0b3269255ac9d0b75
CodeWidGuru <180097088+Code-wid-Guru@users.noreply.github.com>
2025-10-16 02:06:57 +0530

    in progress experience page

--- webapp/controller/Experience.controller.js
+++ webapp/controller/Experience.controller.js
@@ -2,5 +2,18 @@ sap.ui.define([
   "sap/ui/core/mvc/Controller"
 ], function (Controller) {
   "use strict";
-  return Controller.extend("SapDevPortfolio.controller.Experience", {});
+  return Controller.extend("SapDevPortfolio.controller.Experience", {
+    onPress: function (oEvent) {
+      var oItem = oEvent.getSource();
+      var oCtx = oItem.getBindingContext("Experience");
+      var oExperience = oCtx.getObject();
+      const oModel = new sap.ui.model.json.JSONModel(oExperience.details);
+      var temp = this.getView().setModel(oModel, "detailsModel");
+      console.log(this.getView());
+      console.log(temp);
+      // var oRightSplitArea = this.getView().byId("fillmedaddy");
+      // // oRightSplitArea.setText(oExperience.details);
+      // oRightSplitArea.
+    }
+  });
 });
